01bbc5b6e788b681e583e78f765cf20e15dc29b3
d <dddddddtpls@gmail.com>
2024-08-20 16:16:04 +0300

    do a lot of things

--- dpUI/src/dpUI/DProgressBar.java
+++ dpUI/src/dpUI/DProgressBar.java
@@ -5,7 +5,8 @@ import processing.core.*;
 
 public class DProgressBar {
 	public PApplet p;
-	public int x, y, w, h, borderColor = 0xFF00FF00, progressColor = 0xFF00FF00, interiorColor = 0xFF003300;
+	public int x, y, w, h, borderColor = 0xFF00FF00, progressColor = 0xFF00FF00, interiorColor = 0xFF003300,
+			textColor = 0xFFFFFFFF;
 	public boolean visible = true, appendProgressToText = true;
 	public byte progress = 0, indentation = 2, borderSize = 1, maxSteps = 100;
 	public String text = "";
@@ -18,37 +19,26 @@ public class DProgressBar {
 		this.w = w;
 		this.h = h;
 	}
-	  public void render() {
-		    if (!visible) return;
-		    p.background(0);
-		    this.interiorW = this.w-this.indentation*2;
-		    this.interiorH = this.h-this.indentation*2;
-		    byte oldProgress = this.progress;
-		    this.progress *= 100/(int)(maxSteps);
-		    p.fill(0, 0);
-		    p.stroke(this.borderColor);
-		    p.strokeWeight(this.borderSize);
-		    p.rect(this.x, this.y, this.w, this.h);
-		    p.noStroke();
-		    p.fill(this.interiorColor);
-		    p.rect(this.x+this.indentation, this.y+this.indentation, this.w-this.indentation*2, this.h-this.indentation*2);
-		    p.textAlign(PApplet.CENTER, PApplet.CENTER);
-		    p.fill(this.progressColor);
-		    p.text(this.text + (this.appendProgressToText ? + this.progress + "%" : ""), this.w/2+this.x, this.h/2+this.y);
-		    p.loadPixels();
-		    try { 
-		      for (int y = 0; y < this.interiorH; y++) {
-		        for (int x = 0; x < (int)(PApplet.lerp(0, this.interiorW, (float)(this.progress)/100)); x++) {
-		          int i = x+this.x+this.indentation+(y+this.y+this.indentation)*p.width;
-		          if (p.pixels[i] == this.progressColor) p.pixels[i] = this.interiorColor;
-		          else if (p.pixels[i] == this.interiorColor) p.pixels[i] = this.progressColor;
-		        }
-		      }
-		    } catch (ArrayIndexOutOfBoundsException e) {
-		      ShowMessage.showError(e+" on rendering progressBar", false);
-		    }
-		    p.updatePixels();
-		    p.textAlign(PApplet.LEFT, PApplet.BOTTOM);
-		    this.progress = (byte)(oldProgress+1);
-		  }
+
+	public void render() {
+		if (!visible) {
+			return;
+		}
+		p.background(0);
+		interiorW = w - indentation * 2;
+		interiorH = h - indentation * 2;
+		progress *= 100 / (maxSteps);
+		p.fill(0, 0);
+		p.stroke(borderColor);
+		p.strokeWeight(borderSize);
+		p.rect(x, y, w, h);
+		p.noStroke();
+		p.fill(interiorColor);
+		p.rect(x + indentation, y + indentation, w - indentation * 2, h - indentation * 2);
+		p.textAlign(PConstants.CENTER, PConstants.CENTER);
+		p.fill(textColor);
+		p.text(text + (appendProgressToText ? +progress + "%" : ""), w / 2 + x, h / 2 + y);
+		p.rect(x + indentation, y + indentation, PApplet.lerp(0, interiorW, (float) (progress) / 100), interiorH);
+		p.textAlign(PConstants.LEFT, PConstants.BOTTOM);
+	}
 }
