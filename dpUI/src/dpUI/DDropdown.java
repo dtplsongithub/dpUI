75f6a92ed7879bd3236bd07633146d6a2957ebbf
d <dddddddtpls@gmail.com>
2024-08-26 08:48:29 +0300

    i was using wrong values

--- dpUI/src/dpUI/DDropdown.java
+++ dpUI/src/dpUI/DDropdown.java
@@ -8,7 +8,7 @@ public class DDropdown extends DMenu {
 	public boolean extended = false;
 	public DDropdownStyle s = DDropdownStyle.LEFT;
 	public String summary = "";
-	public int x = 0, y = 0, w = 0, h = 0, summaryBackgroundColor = 0xff111111, summaryTextColor = 0xffffffff;
+	public int dx = 0, dy = 0, dw = 0, dh = 0, summaryBackgroundColor = 0xff111111, summaryTextColor = 0xffffffff;
 
 	public DDropdown(PApplet p, int x, int y, int w, int h, int contentH, String summary) {
 		super(p, x, y, w, contentH);
@@ -19,16 +19,16 @@ public class DDropdown extends DMenu {
 		p.pushStyle();
 		p.noStroke();
 		p.fill(summaryBackgroundColor);
-		p.rect(x, y, w, h);
+		p.rect(dx, dy, dw, dh);
 		p.fill(summaryTextColor);
 		p.textAlign(PConstants.CENTER, PConstants.CENTER);
 		p.strokeWeight(5);
 		p.strokeJoin(PConstants.MITER);
 		p.strokeCap(PConstants.ROUND);
-		int sxo = x + (s==DDropdownStyle.RIGHT?w-16:0), syo = y + h/2-8;
-		if (s == DDropdownStyle.LEFT) p.text(summary, x + w / 2 + 8, y + h / 2);
-		if (s == DDropdownStyle.RIGHT) p.text(summary, x + w / 2 - 8 , y + h / 2);
-		if (s == DDropdownStyle.INVIZ) p.text(summary, x + w / 2, y + h / 2);
+		int sxo = dx + (s==DDropdownStyle.RIGHT?w-16:0), syo = dy + dh/2-8;
+		if (s == DDropdownStyle.LEFT) p.text(summary, dx + dw / 2 + 8, dy + dh / 2);
+		if (s == DDropdownStyle.RIGHT) p.text(summary, dx + dw / 2 - 8 , dy + dh / 2);
+		if (s == DDropdownStyle.INVIZ) p.text(summary, dx + dw / 2, dy + dh / 2);
 		p.stroke(summaryTextColor);
 		if (s == DDropdownStyle.INVIZ) p.noStroke();
 		p.beginShape();
