90dee84c5cccf7851841a07eca42c241337b478f
d <dddddddtpls@gmail.com>
2024-09-03 19:01:19 +0300

    website prototype!

--- src/DDropdown.java
+++ src/DDropdown.java
@@ -2,11 +2,15 @@ import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
+@SuppressWarnings("unused")
 public class DDropdown extends DInteractive{
 	public boolean extended;
 	public DDropdownStyle style = DDropdownStyle.LEFT;
-	public int contentW = 100, contentH = 100, summaryBackgroundColor = 0xff222222, summaryTextColor;
+	public int contentW = 100, contentH = 100, summaryBackgroundColor = 0xff222222, summaryTextColor = 0xffffffff;
 	
+	public DDropdown (PApplet c, int x, int y, int w, int h, int contentW, int contentH) {
+		
+	}
 
 	public void render(PGraphics p, int scrollY, int px, int py) {
 	
